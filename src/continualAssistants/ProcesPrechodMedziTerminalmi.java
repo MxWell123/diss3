3260d18d8f26d34b31db72d0412cf353ea9be4ff
Dávid Klubert <davidklubert@gmail.com>
2018-04-29 14:07:48 +0200

    hotovo proces Nastup zakaznika

--- src/continualAssistants/ProcesPrechodMedziTerminalmi.java
+++ src/continualAssistants/ProcesPrechodMedziTerminalmi.java
@@ -6,54 +6,84 @@ import agents.*;
 import OSPABA.Process;
 
 //meta! id="103"
-public class ProcesPrechodMedziTerminalmi extends Process
-{
-	public ProcesPrechodMedziTerminalmi(int id, Simulation mySim, CommonAgent myAgent)
-	{
-		super(id, mySim, myAgent);
-	}
+public class ProcesPrechodMedziTerminalmi extends Process {
 
-	@Override
-	public void prepareReplication()
-	{
-		super.prepareReplication();
-		// Setup component for the next replication
-	}
+    private static final double PRECHOD_TERM1_TERM2 = (0.5 / 35.00) * 3600;
+    private static final double PRECHOD_TERM2_ARCAR = (3.4 / 35.00) * 3600;
+    private static final double PRECHOD_ARCAR_TERM3 = (2.9 / 35.00) * 3600;
+    private static final double PRECHOD_TERM3_TERM1 = (0.9 / 35.00) * 3600;
+    private static final double PRECHOD_ARCAR_TERM1 = (2.5 / 35.00) * 3600;
 
-	//meta! sender="AgentMinibus", id="104", type="Start"
-	public void processStart(MessageForm message)
-	{
-	}
+    public ProcesPrechodMedziTerminalmi(int id, Simulation mySim, CommonAgent myAgent) {
+        super(id, mySim, myAgent);
+    }
 
-	//meta! userInfo="Process messages defined in code", id="0"
-	public void processDefault(MessageForm message)
-	{
-		switch (message.code())
-		{
-		}
-	}
+    @Override
+    public void prepareReplication() {
+        super.prepareReplication();
+        // Setup component for the next replication
+    }
 
-	//meta! userInfo="Generated code: do not modify", tag="begin"
-	@Override
-	public void processMessage(MessageForm message)
-	{
-		switch (message.code())
-		{
-		case Mc.start:
-			processStart(message);
-		break;
+    //meta! sender="AgentMinibus", id="104", type="Start"
+    public void processStart(MessageForm message) {
+        MyMessage sprava = (MyMessage) message;
+        if (sprava.getMinibus().getPolohaMinibusu() == 0) {
+            sprava.getMinibus().setPolohaMinibusu(1);
+            sprava.setCode(Mc.koniecPrechodu);
+            hold(PRECHOD_TERM1_TERM2, message);
+        } else if (sprava.getMinibus().getPolohaMinibusu() == 1) {
+            sprava.getMinibus().setPolohaMinibusu(2);
+            sprava.setCode(Mc.koniecPrechodu);
+            hold(PRECHOD_TERM2_ARCAR, message);
+        } else if (sprava.getMinibus().getPolohaMinibusu() == 2) {
+            if (!sprava.getMinibus().jeMinibusPrazdny()) { // ide do term3
+                sprava.getMinibus().setPolohaMinibusu(3);
+                sprava.setCode(Mc.koniecPrechodu);
+                hold(PRECHOD_ARCAR_TERM3, message);
+            } else {
+                sprava.getMinibus().setPolohaMinibusu(0); // ide rovno term1
+                sprava.setCode(Mc.koniecPrechodu);
+                hold(PRECHOD_ARCAR_TERM1, message);
+            }
+        } else if (sprava.getMinibus().getPolohaMinibusu() == 3) {
+            sprava.getMinibus().setPolohaMinibusu(0);
+            sprava.setCode(Mc.koniecPrechodu);
+            hold(PRECHOD_TERM3_TERM1, message);
+        }
+    }
 
-		default:
-			processDefault(message);
-		break;
-		}
-	}
-	//meta! tag="end"
+    //meta! userInfo="Process messages defined in code", id="0"
+    public void processDefault(MessageForm message) {
+        throw new UnsupportedOperationException("Vykonal sa default v ProcesPrechodMedziTerminalmi.");
+    }
 
-	@Override
-	public AgentMinibus myAgent()
-	{
-		return (AgentMinibus)super.myAgent();
-	}
+    //meta! sender="AgentMinibus", id="178", type="Notice"
+    public void processKoniecPrechodu(MessageForm message) {
+        assistantFinished(message);
+    }
+
+    //meta! userInfo="Generated code: do not modify", tag="begin"
+    @Override
+    public void processMessage(MessageForm message) {
+        switch (message.code()) {
+            case Mc.start:
+                processStart(message);
+                break;
+
+            case Mc.koniecPrechodu:
+                processKoniecPrechodu(message);
+                break;
+
+            default:
+                processDefault(message);
+                break;
+        }
+    }
+    //meta! tag="end"
+
+    @Override
+    public AgentMinibus myAgent() {
+        return (AgentMinibus) super.myAgent();
+    }
 
 }
